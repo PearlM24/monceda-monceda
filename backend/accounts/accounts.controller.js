503a0009a15975d22f0a3bbc49b9af7d12da2098
unknown <HH832@ccs.local>
2025-05-17 19:26:14 +0800

    Updating file

--- backend/accounts/accounts.controller.js
+++ backend/accounts/accounts.controller.js
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿const express = require('express');
+﻿﻿const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 const validateRequest = require('_middleware/validate-request');
@@ -12,7 +12,7 @@ router.post('/authenticate', authenticateSchema, authenticate);
 router.post('/refresh-token', refreshToken);
 router.post('/revoke-token', authorize(), revokeTokenSchema, revokeToken);
 router.post('/register', registerSchema, register);
-router.post('/verify-email', verifyEmailSchema, );  //verifyEmail);
+router.post('/verify-email', verifyEmailSchema, );//verifyEmail);
 router.post('/forgot-password', forgotPasswordSchema, forgotPassword);
 router.post('/validate-reset-token', validateResetTokenSchema, validateResetToken);
 router.post('/reset-password', resetPasswordSchema, resetPassword);
