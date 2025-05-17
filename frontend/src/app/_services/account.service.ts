f700e4c704a9bbfbeb006b847527d7143ef0f10f
unknown <HH832@ccs.local>
2025-05-17 19:44:16 +0800

    Trying to remove the verification

--- frontend/src/app/_services/account.service.ts
+++ frontend/src/app/_services/account.service.ts
@@ -1,4 +1,4 @@
-﻿﻿import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
